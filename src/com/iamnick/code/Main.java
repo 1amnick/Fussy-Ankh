9b184d8f20d70249f376c47cd7b9e9a0e43e46f2
1amnick <nic1115v@gmail.com>
2017-01-06 00:40:39 -0500

    misc stuff

--- src/com/iamnick/code/Main.java
+++ src/com/iamnick/code/Main.java
@@ -2,11 +2,9 @@ package com.iamnick.code;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Main {
 
